a9df703ded2264ca53230b814f1099eeec03a61d
Leah Hanson <leahahanson@gmail.com>
2015-07-01 23:06:24 +0200

    Improve comments and formatting.
    The biggest change here is the addition of a big comment at the top,
    talking about the module over all.

--- src/WebSockets.jl
+++ src/WebSockets.jl
@@ -1,5 +1,21 @@
 module WebSockets
 
+# This module implements the server side of the WebSockets protocol. Some
+# things would need to be added to implement a WebSockets client, such as
+# masking of sent frames.
+#
+# WebSockets expects to be used with HttpServer to provide the HttpServer
+# for accepting the HTTP request that begins the opening handshake. WebSockets
+# implements a subtype of the WebSocketInterface from HttpServer; this means
+# that you can create a WebSocketsHandler and pass it into the constructor for
+# an http server.
+#
+# Future improvements:
+# 1. Logging of refused requests and closures due to bad behavior of client.
+# 2. Better error handling (should we always be using "error"?)
+# 3. Unit tests with an actual client -- to automatically test the examples.
+# 4. Send close messages with status codes.
+# 5. Allow users to receive control messages if they want to.
 using HttpCommon
 using HttpServer
 using Codecs
@@ -12,11 +28,8 @@ export WebSocket,
        send_ping,
        send_pong
 
-import Base: read, write, isopen, close
-
-# A WebSocket is just a wrapper over a TcpSocket
-# All it does is wrap outgoing data in the protocol
-# and unwrapp incoming data.
+# A WebSocket is a wrapper over a TcpSocket. It takes care of wrapping outgoing
+# data in a frame and unwrapping (and concatenating) incoming data.
 type WebSocket
   id::Int
   socket::Base.TcpSocket
@@ -28,10 +41,8 @@ type WebSocket
   end
 end
 
+# WebSocket Frames
 #
-# WebSocket Packets
-#
-
 #      0                   1                   2                   3
 #      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 #     +-+-+-+-+-------+-+-------------+-------------------------------+
@@ -51,7 +62,6 @@ end
 #     |                     Payload Data continued ...                |
 #     +---------------------------------------------------------------+
 #
-
 # Opcode values
 #  *  %x0 denotes a continuation frame
 #  *  %x1 denotes a text frame
@@ -63,9 +73,7 @@ end
 #  *  %xA denotes a pong
 #  *  %xB-F are reserved for further control frames
 
-# Internal function for wrapping one
-# piece of data into a WS header
-# sending it out over the TcpSocket.
+# Constructs a frame from the arguments and sends it on the provided socket.
 function send_fragment(ws::WebSocket, islast::Bool, data::Array{Uint8}, opcode=0b0001)
   l = length(data)
   b1::Uint8 = (islast ? 0b1000_0000 : 0b0000_0000) | opcode
@@ -87,41 +95,42 @@ function send_fragment(ws::WebSocket, islast::Bool, data::Array{Uint8}, opcode=0
     error("Attempted to send too much data for one websocket fragment\n")
   end
 end
-send_fragment(ws::WebSocket, islast::Bool, data::ByteString, opcode=0b0001) =
+
+# A version of send_fragment for text data.
+function send_fragment(ws::WebSocket, islast::Bool, data::ByteString, opcode=0b0001)
   send_fragment(ws, islast, data.data, opcode)
+end
 
-# Exported function for sending data into a websocket
-# data should allow length(data) and write(TcpSocket,data)
-# all protocol details are taken care of.
-function write(ws::WebSocket,data::ByteString)
+# Write text data; will be sent as one frame.
+function Base.write(ws::WebSocket,data::ByteString)
+  if ws.is_closed
+    @show ws
+    error("Attempted write to closed WebSocket\n")
+  end
+  send_fragment(ws, true, data)
+end
+
+# Write binary data; will be sent as one frmae.
+function Base.write(ws::WebSocket, data::Array{Uint8})
   if ws.is_closed
     @show ws
     error("attempt to write to closed WebSocket\n")
   end
-
-  #assume data fits in one fragment
-  send_fragment(ws,true,data)
+  send_fragment(ws, true, data, 0b0010)
 end
 
-function write(ws::WebSocket, data::Array{Uint8})
-  if ws.is_closed
-    @show ws
-    error("attempt to write to closed WebSocket\n")
-  end
-
-  #indicate this is binary data
-  send_fragment(ws,true,data, 0b0010)
-end
-
+# Send a ping message, optionally with data.
 function send_ping(ws::WebSocket, data = "")
   send_fragment(ws, true, data, 0x9)
 end
 
+# Send a pong message, optionally with data.
 function send_pong(ws::WebSocket, data = "")
   send_fragment(ws, true, data, 0xA)
 end
 
-function close(ws::WebSocket)
+# Send a close message.
+function Base.close(ws::WebSocket)
     send_fragment(ws, true, "", 0x8)
     ws.is_closed = true
     while true
@@ -133,24 +142,25 @@ function close(ws::WebSocket)
     close(ws.socket)
 end
 
-isopen(ws::WebSocket) = !ws.is_closed
+# A WebSocket is closed if the underlying TCP socket closes, or if we send or
+# receive a close message.
+Base.isopen(ws::WebSocket) = !ws.is_closed
 
 
-# represents one received message fragment
-# (headers + data)
+# Represents one (received) message frame.
 type WebSocketFragment
   is_last::Bool
   rsv1::Bool
   rsv2::Bool
   rsv3::Bool
-  opcode::Uint8 #really, Uint4
+  opcode::Uint8  # This is actually a Uint4 value.
   is_masked::Bool
   payload_len::Uint64
-  maskkey::Vector{Uint8} #Union{Array{Uint8,4}, Nothing}
-  data::Vector{Uint8} #ByteString
+  maskkey::Vector{Uint8}  # This will be 4 bytes on frames from the client.
+  data::Vector{Uint8}  # For text messages, this is a ByteString.
 end
 
-# constructor to do some conversions from bits to Bool.
+# This constructor handles conversions from bytes to bools.
 function WebSocketFragment(
    fin::Uint8
   ,rsv1::Uint8
@@ -174,24 +184,20 @@ function WebSocketFragment(
     , data)
 end
 
-# A message frame/fragment can be
-# either a control frame or a data frame
-# this function determines which it is
-# according to the opcode.
-# if that bit is set (1), then this is a control frame.
+# Control frames have opcodes with the highest bit = 1.
 is_control_frame(msg::WebSocketFragment) = (msg.opcode & 0b0000_1000) > 0
 
 # Respond to pings, ignore pongs, respond to close.
 function handle_control_frame(ws::WebSocket,wsf::WebSocketFragment)
-  if wsf.opcode == 0x8 #  %x8 denotes a connection close
+  if wsf.opcode == 0x8  # %x8 denotes a connection close.
     send_fragment(ws, true, "", 0x8)
     ws.is_closed = true
     wait(ws.socket.closenotify)
-  elseif wsf.opcode == 0x9 #  %x9 denotes a ping
+  elseif wsf.opcode == 0x9  # %x9 denotes a ping.
     send_pong(ws,wsf.data)
-  elseif wsf.opcode == 0xA #  %xA denotes a pong
-    # Nothing to do here
-  else #  %xB-F are reserved for further control frames
+  elseif wsf.opcode == 0xA  # %xA denotes a pong.
+    # Nothing to do here; no reply is needed for a pong message.
+  else  # %xB-F are reserved for further control frames
     error("Unknown opcode $(wsf.opcode)")
   end
 end
@@ -200,11 +206,11 @@ end
 function read_frame(ws::WebSocket)
   a = read(ws.socket,Uint8)
   fin    = a & 0b1000_0000 >>> 7  # If fin, then is final fragment
-  rsv1   = a & 0b0100_0000 #if not 0, fail.
-  rsv2   = a & 0b0010_0000 #if not 0, fail.
-  rsv3   = a & 0b0001_0000 #if not 0, fail.
-  opcode = a & 0b0000_1111 #if not known code, fail.
-  # TODO: add validation somewhere to ensure rsv,opcode,mask,etc are valid.
+  rsv1   = a & 0b0100_0000  # If not 0, fail.
+  rsv2   = a & 0b0010_0000  # If not 0, fail.
+  rsv3   = a & 0b0001_0000  # If not 0, fail.
+  opcode = a & 0b0000_1111  # If not known code, fail.
+  # TODO: add validation somewhere to ensure rsv, opcode, mask, etc are valid.
 
   b = read(ws.socket,Uint8)
   mask = b & 0b1000_0000 >>> 7  # If not 1, fail.
@@ -231,11 +237,13 @@ function read_frame(ws::WebSocket)
   return WebSocketFragment(fin,rsv1,rsv2,rsv3,opcode,mask,payload_len,maskkey,data)
 end
 
-import Base.read
-# Read data from a WebSocket.
-# This will block until a full message has been received.
-# The headers will be stripped and only the data will be returned.
-function read(ws::WebSocket)
+# Read one non-control message from a WebSocket. Any control messages that are
+# read will be handled by the handle_control_frame function. This function will
+# not return until a full non-control message has been read. If the other side
+# doesn't ever complete it's message, this function will never return. Only the
+# data (contents/body/payload) of the message will be returned from this
+# function.
+function Base.read(ws::WebSocket)
   if ws.is_closed
     error("Attempt to read from closed WebSocket")
   end
@@ -261,9 +269,12 @@ end
 # WebSocket Handshake
 #
 
-# Transforms a websocket client key into the server's accept value.
-# Concatenate key with magic string from RFC, then SHA1 hash.
-# Return base64 encoded string of value.
+# This function transforms a websocket client key into the server's accept
+# value. This is done in three steps:
+#   1. Concatenate key with magic string from RFC.
+#   2. SHA1 hash the resulting base64 string.
+#   3. Encode the resulting number in base64.
+# This function then returns the string of the base64-encoded value.
 function generate_websocket_key(key)
   h = HashState(SHA1)
   update!(h, key*"258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
@@ -272,8 +283,7 @@ end
 
 # Responds to a WebSocket handshake request.
 # Checks for required headers; sends Response(400) if they're missing or bad.
-# Otherwise, ransforms client key into accept value, and sends Reponse(101).
-# TODO: Add logging of refused requests
+# Otherwise, transforms client key into accept value, and sends Reponse(101).
 function websocket_handshake(request,client)
   if !haskey(request.headers, "Sec-WebSocket-Key")
     Base.write(client.sock, Response(400))
