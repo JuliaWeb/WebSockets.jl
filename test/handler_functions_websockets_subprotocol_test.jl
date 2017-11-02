206a0c76e311bdb2b9f01da3159f223ac1fd7fa5
hustf <hustf@users.noreply.github.com>
2017-11-02 20:11:45 +0100

    modified:   .travis.yml                  No test on 0.7, fails due to httpcommon. 0.6 included. modified:   appveyor.yml                 Installs chrome, firefox. Precompiles httpserver with no depwarns. new file:   test/REQUIRE                 0.5 minimum for testing. Still 0.4 for use. modified:   test/runtests.jl			 Includes browsertest.jl new file:   test/favicon.ico             Tab icon for non-headless browsers. new file:   test/browsertest.html        Web page with three websockets: General, subprotocol and bad protocol. new file:   test/browsertest.jl          < runtests.jl. Launches up to five browsers. Tests websockets on the browsers which requests browsertest. new file:   test/functions_open_browsers.jl   < browsertest. Launches available browsers on localhost browsertest (not tested on unix / linux). new file:   test/phantom.js                     < functions_open_browsers. Shell script for the browser with closedown on timeout, also sets the user-agent. new file:   test/functions_server.jl     < browsertest. A server with websockets and logging. new file:   test/functions_log_test.jl                             < functions_server. Blocking console logs, colorful output for servers & etc. new file:   test/handler_functions_events.jl                       < functions_server. Logs fired events to console. new file:   test/handler_functions_websockets_general_test.jl      < functions_server. Stores ws messages and sends replies. new file:   test/handler_functions_websockets_subprotocol_test.jl  < functions_server. Store ws messages and replies.

---
+++ test/handler_functions_websockets_subprotocol_test.jl
@@ -0,0 +1,54 @@
+#     These functions deal with specified websocket protocols.
+#      Included in server_functions.jl
+
+function ws_test_protocol(ws::WebSockets.WebSocket)
+    id = "ws_test_protocol #$(ws.id)\t"
+    WEBSOCKETS[ws.id] = ws
+    WEBSOCKETS_SUBPROTOCOL[ws.id] = ws
+    RECEIVED_WS_MSGS[ws.id] = String[]
+    clog(id,  " Entering to read websocket.\n")
+    stri = wsmsg_general(ws)
+    clog(id,  " Send a message\n")
+    push!(RECEIVED_WS_MSGS[ws.id], stri)
+    writeto(ws.id, "Here, have a message!")
+    clog(id,  " Continue to listen for message\n")
+    while isopen(ws)
+        stri = wsmsg_testprotocol(ws)
+        push!(RECEIVED_WS_MSGS[ws.id], stri)
+    end
+    clog(id, "Websocket was closed; exiting read loop\n")
+    nothing
+end
+
+"""
+Listens for the next websocket message.
+"""
+function wsmsg_testprotocol(ws::WebSockets.WebSocket)
+    id = "wsmsg_testprotocol #$(ws.id)\t"
+    clog(id,  " Entering to read websocket.\n")
+    stri = ""
+    try
+        # Holding here. Cleanup code could work with InterruptException and Base.throwto.
+        # This is not considered necessary for the test (and doesn't really work well).
+        # So, this may continue running after the test is finished.
+        stri = ws|> read  |> String
+        clog(id, "Received: \n", :yellow, :bold, stri, "\n")
+    catch e
+        if typeof(e) == WebSockets.WebSocketClosedError
+            clog(id, :green, " Websocket was or is being closed.\n")
+        else
+            clog(id, "Caught exception..\n", "\t\t", Base.error_color(), e, "\n")
+            if isopen(ws)
+                clog(id, "Closing\n")
+                close(ws)
+            end
+        end
+    end
+    clog(id, "Exiting\n")
+    return stri
+end
+
+
+
+
+return nothing
