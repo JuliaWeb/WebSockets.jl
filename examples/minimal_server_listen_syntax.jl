55118adc4b01af9cc5a81f0f96689399c6029d7a
hustf <hustf@users.noreply.github.com>
2018-09-01 19:12:43 +0200

    Examples with alternative syntax for servers 	new file:   examples/minimal_server_listen_do_syntax.jl 	new file:   examples/minimal_server_listen_syntax.jl

---
+++ examples/minimal_server_listen_syntax.jl
@@ -0,0 +1,57 @@
+# Minimal server using the 'listen' syntax, starting with the 'inner' functions
+const BAREHTML = "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">
+ <title>Empty.html</title></head><body></body></html>"
+import Sockets
+using WebSockets
+import HTTP.listen
+import HTTP.Servers.handle_request
+const LOCALIP = string(Sockets.getipaddr())
+const PORT = 8080
+const BODY =  "<body><p>Press F12
+                <p>ws = new WebSocket(\"ws://$LOCALIP:$PORT\")
+                <p>ws.onmessage = function(e){console.log(e.data)}
+                <p>ws.send(\"Browser console says hello!\")
+                </body>"
+function coroutine(ws)
+    while isopen(ws)
+        data, = readguarded(ws)
+        s = String(data)
+        if s == ""
+            writeguarded(ws, "Goodbye!")
+            break
+        end
+        println("Received: ", s)
+        writeguarded(ws, "Hello! Send empty message to exit, or just leave.")
+    end
+end
+
+function gatekeeper(req, ws)
+    orig = WebSockets.origin(req)
+    println("\nOrigin:", orig, "    Target:", target(req), "    subprotocol:", subprotocol(req))
+    if occursin(LOCALIP, orig)
+        coroutine(ws)
+    elseif orig == ""
+        @info "Non-browser clients don't send Origin. We liberally accept the update request in this case."
+        coroutine(ws)
+    else
+        @warn "Inacceptable request"
+    end
+end
+
+handle(req) = replace(BAREHTML, "<body></body>" => BODY) |> WebSockets.Response
+global TCPREF = Ref{Base.IOServer}()
+@info("Browser > $LOCALIP:$PORT , F12> console > ws = new WebSocket(\"ws://$LOCALIP:$PORT\") ")
+try
+    listen(LOCALIP, UInt16(PORT), tcpref = TCPREF) do http
+        if WebSockets.is_upgrade(http.message)
+            WebSockets.upgrade(gatekeeper, http)
+        else
+            handle_request(handle, http)
+        end
+    end
+catch err
+    # Add your own error handling code; HTTP.jl sends error code to the client.
+    @info err
+    @info stacktrace(catch_backtrace())[1:4]
+end
+nothing
