97e0a241255cb7893299d337d72fede60c500214
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-22 18:08:03 +0000

    allow IPAddr in serve

--- src/HTTP.jl
+++ src/HTTP.jl
@@ -322,6 +322,9 @@ function ServerWS(h::Function, w::Function; kwargs...)
     ServerWS(RequestHandlerFunction(h), WSHandlerFunction(w); kwargs...)
 end
 
+to_IPAddr(ip::IPAddr) = ip
+to_IPAddr(s::AbstractString) = parse(IpAddr, s)
+
 """
     WebSockets.serve(server::ServerWS, port)
     WebSockets.serve(server::ServerWS, host, port)
@@ -347,7 +350,7 @@ After a suspected connection task failure:
 """
 function serve(serverws::ServerWS, host, port, verbose)
     # An internal reference used for closing.
-    tcpserver = Ref{Union{IOServer, Nothing}}(Sockets.listen(InetAddr(parse(IPAddr, host), port)))
+    tcpserver = Ref{Union{IOServer, Nothing}}(Sockets.listen(InetAddr(to_IPAddr(host), port)))
     # Start a couroutine that sleeps until something is put on the .in channel.
     @async begin
          # Next line will hold until something is put on the channel
